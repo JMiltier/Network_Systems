832279de4105f6a8cf3ac2e99fb388f474250254
Joshua T. Miltier <jomi3524@colorado.edu>
2020-09-16 21:28:20 -0600

    clean client and server code

--- UDP_Sockets/udp/udp_client.c
+++ UDP_Sockets/udp/udp_client.c
@@ -1,7 +1,8 @@
-/* 
- * udpclient.c - A simple UDP client
- * usage: udpclient <host> <port>
- */
+/* ***********************************
+ * udp_client.c - A simple UDP client
+ * usage: ./udp_client <host> <port>
+ *********************************** */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,13 +10,11 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h> 
+#include <netdb.h>
 
 #define BUFSIZE 1024
 
-/* 
- * error - wrapper for perror
- */
+//! error - wrapper for perror
 void error(char *msg) {
     perror(msg);
     exit(0);
@@ -29,7 +28,7 @@ int main(int argc, char **argv) {
     char *hostname;
     char buf[BUFSIZE];
 
-    /* check command line arguments */
+    // check command line arguments
     if (argc != 3) {
        fprintf(stderr,"usage: %s <hostname> <port>\n", argv[0]);
        exit(0);
@@ -37,39 +36,39 @@ int main(int argc, char **argv) {
     hostname = argv[1];
     portno = atoi(argv[2]);
 
-    /* socket: create the socket */
+    // socket: create the socket
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) 
+    if (sockfd < 0)
         error("ERROR opening socket");
 
-    /* gethostbyname: get the server's DNS entry */
+    // gethostbyname: get the server's DNS entry
     server = gethostbyname(hostname);
     if (server == NULL) {
         fprintf(stderr,"ERROR, no such host as %s\n", hostname);
         exit(0);
     }
 
-    /* build the server's Internet address */
+    // build the server's Internet address
     bzero((char *) &serveraddr, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
+    bcopy((char *)server->h_addr,
 	  (char *)&serveraddr.sin_addr.s_addr, server->h_length);
     serveraddr.sin_port = htons(portno);
 
-    /* get a message from the user */
+    // get a message from the user
     bzero(buf, BUFSIZE);
     printf("Please enter msg: ");
     fgets(buf, BUFSIZE, stdin);
 
-    /* send the message to the server */
+    // send the message to the server
     serverlen = sizeof(serveraddr);
     n = sendto(sockfd, buf, strlen(buf), 0, &serveraddr, serverlen);
-    if (n < 0) 
+    if (n < 0)
       error("ERROR in sendto");
-    
-    /* print the server's reply */
+
+    // print the server's reply
     n = recvfrom(sockfd, buf, strlen(buf), 0, &serveraddr, &serverlen);
-    if (n < 0) 
+    if (n < 0)
       error("ERROR in recvfrom");
     printf("Echo from server: %s", buf);
     return 0;
