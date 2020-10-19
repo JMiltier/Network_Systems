7e18e5f2bb87a8e9d510ff8f14004fcaa9d697e9
Joshua T. Miltier <jomi3524@colorado.edu>
2020-10-19 13:43:34 -0600

    webserver init and makefile

--- HTTP_Web_Server/httpechosrv.c
+++ HTTP_Web_Server/httpechosrv.c
@@ -56,7 +56,7 @@ void * thread(void * vargp) {
 void echo(int connfd) {
     size_t n;
     char buf[MAXLINE];
-    char httpmsg[]="HTTP/1.1 200 Document Follows\r\nContent-Type:text/html\r\nContent-Length:32\r\n\r\n<html><h1>Hello CSCI4273 Course!</h1>";
+    char httpmsg[]="HTTP/1.1 200 Document Follows\r\nContent-Type:text/html\r\nContent-Length:32\r\n\r\n<html><h1>Hello World!</h1>";
 
     n = read(connfd, buf, MAXLINE);
     printf("server received the following request:\n%s\n",buf);
